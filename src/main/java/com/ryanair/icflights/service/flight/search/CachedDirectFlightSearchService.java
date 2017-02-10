03c27772580753a74e2c478cf9a6045d023a5038
Alex Zamkovyi <invaa@Alexs-MacBook-Pro.local>
2017-02-10 17:49:57 +0100

    v1.0.1

--- src/main/java/com/ryanair/icflights/service/flight/search/CachedDirectFlightSearchService.java
+++ src/main/java/com/ryanair/icflights/service/flight/search/CachedDirectFlightSearchService.java
@@ -14,7 +14,6 @@ import org.springframework.cache.annotation.Cacheable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 /**
@@ -41,40 +40,30 @@ public final class CachedDirectFlightSearchService implements FlightSearchServic
             final String to,
             final LocalDateTime departureDateTime,
             final LocalDateTime arrivalDateTime) {
-        final List<CompletableFuture<Boolean>> resultFutureArray
-                = new ArrayList<>();
-        final List<Flight> flights = new CopyOnWriteArrayList<>();
+        final List<Flight> flights = new ArrayList<>();
         for (int i = 0; i <= periodService.getMonthsChangesAmount(departureDateTime, arrivalDateTime); i++) {
             final LocalDate flightsDate = departureDateTime.toLocalDate().plusMonths(i);
-            final CompletableFuture<MonthTimetableDto> clientResponseFuture = CompletableFuture.supplyAsync(()
-                    -> timetableServiceClient.getMonthTimetable(
-                                from,
-                                to,
-                                flightsDate.getYear(),
-                                flightsDate.getMonth().getValue()
-            ));
-            final CompletableFuture<Boolean> mappingFuture =
-                    clientResponseFuture
-                         .thenApply(r ->
-                                    timetableMapper.mapDtoToFlights(
-                                        r,
-                                        from,
-                                        to,
-                                        flightsDate
-                                )
-                        )
-                        .thenApply(r ->
-                                this.filterUnsuitable(r, departureDateTime, arrivalDateTime))
-                        .thenApply(flights::addAll);
-                resultFutureArray.add(mappingFuture);
-        }
-        try {
-            CompletableFuture.allOf(
-                    resultFutureArray.
-                            toArray(new CompletableFuture[0])
-            ).get(serviceConfig.getTimetableWaitTimeout(), TimeUnit.MILLISECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            log.warn("There were an error during the flight search.", e);
+            final MonthTimetableDto monthTimetableDto
+                    = timetableServiceClient.getMonthTimetable(
+                        from,
+                        to,
+                        flightsDate.getYear(),
+                        flightsDate.getMonth().getValue()
+            );
+            final List<Flight> currentFLights
+                    = timetableMapper.mapDtoToFlights(
+                        monthTimetableDto,
+                        from,
+                        to,
+                        flightsDate
+            );
+            flights.addAll(
+                    filterUnsuitable(
+                        currentFLights,
+                        departureDateTime,
+                        arrivalDateTime
+                    )
+            );
         }
         return flights;
     }
@@ -85,16 +74,22 @@ public final class CachedDirectFlightSearchService implements FlightSearchServic
             final LocalDateTime arrivalDateTime
     ) {
         return flights.stream().flatMap(
-                flight -> flight.getLegs()
-                        .stream()
-                .filter(leg ->
-                        leg.getArrivalDateTime().compareTo(arrivalDateTime) <= 0
-                && leg.getDepartureDateTime().compareTo(departureDateTime) >= 0)
-        .map(
-             flightLeg -> new Flight(
-                     0,
-                     Arrays.asList(flightLeg)
-             )
-        )).collect(Collectors.toList());
+              flight -> flight.getLegs()
+                     .stream()
+                     .filter(
+                             leg ->
+                                     leg.getArrivalDateTime()
+                                             .compareTo(arrivalDateTime) <= 0
+                                                && leg.getDepartureDateTime().compareTo(departureDateTime) >= 0
+                     )
+                     .map(
+                             flightLeg -> new Flight(
+                                     0,
+                                     Collections.singletonList(
+                                             flightLeg
+                                     )
+                             )
+                     )
+        ).collect(Collectors.toList());
     }
 }
